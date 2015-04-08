f33870fb8983c1c0e5dc26046601bd7be4b930a7
Frank Carnevale <fjcarnevale@gmail.com>
2015-04-07 20:58:41 -0400

    added index view to show vehicles and miles driven for any fuel ups

--- fueltracker/urls.py
+++ fueltracker/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'fueltracker.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^mpg/', include('mpg.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- mpg/views.py
+++ mpg/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Make, Model, Vehicle, FuelUp
+
 # Create your views here.
+
+def index(request):
+  context = {'vehicles':Vehicle.objects.all()}
+  return render(request, 'mpg/index.html', context)
