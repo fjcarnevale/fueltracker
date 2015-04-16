a97f7d78841ba116454277d193b748474b918323
Frank Carnevale <fjcarnevale@gmail.com>
2015-04-16 18:03:36 -0400

    Index displays makes and known models. Models link to model page which displays all vehicles of that model

--- mpg/urls.py
+++ mpg/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
+  url(r'^model/(?P<model_name>[a-zA-Z0-9]+)$', views.model, name='model'),
 ]
--- mpg/views.py
+++ mpg/views.py
@@ -5,5 +5,11 @@ from .models import Make, Model, Vehicle, FuelUp
 # Create your views here.
 
 def index(request):
-  context = {'vehicles':Vehicle.objects.all()}
+  context = {'makes':Make.objects.all()}
   return render(request, 'mpg/index.html', context)
+
+
+def model(request, model_name):
+  model = Model.objects.get(name__exact=model_name)
+  context = {'model':model}
+  return render(request, 'mpg/model.html', context)
