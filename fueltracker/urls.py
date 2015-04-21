77ae3f004801b14db8206bf1c15c2be5f64d95ca
Frank Carnevale <fjcarnevale@gmail.com>
2015-04-20 20:48:35 -0400

    Started 'authy' app to handle user authentication and registration

--- fueltracker/urls.py
+++ fueltracker/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # url(r'^$', 'fueltracker.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^authy/', include('authy.urls')),
     url(r'^mpg/', include('mpg.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
