64e096bd2148e13966c02516a541c5ddeff9bcab
Frank Carnevale <fjcarnevale@gmail.com>
2015-04-27 23:06:43 -0400

    adding comments to views and models

--- mpg/models.py
+++ mpg/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+# Class for Makes of vehicles
 class Make(models.Model):
   name = models.CharField(max_length=20)
 
   def __str__(self):
     return self.name
 
+# Class for Models of vehicles
 class Model(models.Model):
   make = models.ForeignKey(Make)
   name = models.CharField(max_length=20)
@@ -15,18 +17,24 @@ class Model(models.Model):
   def __str__(self):
     return self.name
 
+  # Calculates the average MPG for all vehicles of this model
   def average_mpg(self):
+    # get all the vehicles of this model
     vehicles = self.vehicle_set.all()
     
+    # gather all the fuel ups
     fuel_ups = []
     for vehicle in vehicles:
       fuel_ups.extend(vehicle.fuelup_set.all())
 
+    # if no fuel ups, return 0
     if not fuel_ups:
       return 0
 
+    # calculate and return the average MPG
     return sum([fu.miles for fu in fuel_ups]) / sum([fu.gallons for fu in fuel_ups])
 
+# Class for Vehicles
 class Vehicle(models.Model):
   model = models.ForeignKey(Model)
   year = models.IntegerField()
@@ -35,6 +43,7 @@ class Vehicle(models.Model):
   def __str__(self):
     return self.name
 
+# Class for FuelUps
 class FuelUp(models.Model):
   date = models.DateField()
   miles = models.IntegerField(default = 0)
--- mpg/views.py
+++ mpg/views.py
@@ -8,78 +8,126 @@ from .models import Make, Model, Vehicle, FuelUp
 
 # Create your views here.
 
+# Convenience function to get the current user
 def get_user(request):
+  # Check if user is authenticated
   if request.user.is_authenticated():
+    # if so, return the user
     user = request.user
   else:
+    # otherwise, return None
     user = None
-  return user
     
+  return user
+  
+# View to handle user authentication
 def login_user(request):
+  # get username and password
   username = request.POST['username']
   password = request.POST['password']
 
+  # authenticate the user
   user = authenticate(username=username, password=password)
 
+  # check that user was authenticated properly
   if user is not None:
     if user.is_active:
+      # if so, log them in
       login(request, user)
 
+  # TODO handle authentication errors
+
+  # redirect to the index
   return HttpResponseRedirect(reverse('index'))
   
+# View to handle logging out users
 def logout_user(request):
-    logout(request)
-    
-    return HttpResponseRedirect(reverse('index'))
+  # logout the user
+  logout(request)
+  
+  # redirect to the index
+  return HttpResponseRedirect(reverse('index'))
 
+# Simple view to show the register page
+def register_page(request):
+  return render(request, 'mpg/register.html')
 
+# View to handle registering users
 def register(request):
+  # Get the user information
   username = request.POST['username']
   email = request.POST['email']
   password = request.POST['password']
 
+  # Create and authenticate the user
   user = User.objects.create_user(username, email, password)
   user = authenticate(username=username, password=password)
 
+  # Check that user was authenticated properly
   if user is not None:
     if user.is_active:
+      # if so, log them in
       login(request, user)
 
+  # redirect to the index
   return HttpResponseRedirect(reverse('index'))
 
-def register_page(request):
-  return render(request, 'mpg/register.html')
-
+# Index view
 def index(request):
+  # Get the user
   user = get_user(request)
+  
+  # Set the context, retrieving all makes
   context = {'user':user, 'makes':Make.objects.all()}
   return render(request, 'mpg/index.html', context)
 
-
-def model(request, model_name):
-  user = get_user(request)
-  model = Model.objects.get(name__exact=model_name)
-  context = {'user':user, 'model':model}
-  return render(request, 'mpg/model.html', context)
-
-
+# Make view
 def make(request, make_name):
+  # get the user
   user = get_user(request)
+  
+  # get the make by name
   make = Make.objects.get(name__exact=make_name)
+  
+  # set the context and render the page
   context = {'user':user, 'make':make}
   return render(request, 'mpg/make.html', context)
 
 
-def vehicle(request, vehicle_id):
+# Model view
+def model(request, model_name):
+  # Get the user
   user = get_user(request)
+  
+  # Get the model based on the name
+  model = Model.objects.get(name__exact=model_name)
+  
+  # Set the context and render the page
+  context = {'user':user, 'model':model}
+  return render(request, 'mpg/model.html', context)
+
+
+# Vehicle view
+def vehicle(request, vehicle_id):
+  # get the user
+  user = get_user(request)
+  
+  # get the vehicle by ID or 404
   vehicle = get_object_or_404(Vehicle, pk=vehicle_id)
+  
+  # set the context and render the page
   context = {'user':user,'vehicle':vehicle}
   return render(request, 'mpg/vehicle.html', context)
 
+# View to add a new make
+# TODO this will become part of the API
 def addMake(request):
+  # get the name of the new make
   make_name = request.POST['make']
   
+  # if a make does not already exist by that name
   if not len(Make.objects.filter(name=make_name)):
+    # create and save the new make
     make = Make()
     make.name = make_name
     make.save()
